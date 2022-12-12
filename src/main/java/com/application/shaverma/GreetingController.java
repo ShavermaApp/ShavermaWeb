8d7af3dcc4ad1f361542690a72a3095ba58ee838
Zaby2 <109461366+Zaby2@users.noreply.github.com>
2022-12-12 17:48:14 +0300

    charset need to be fixed

--- src/main/java/com/application/shaverma/GreetingController.java
+++ src/main/java/com/application/shaverma/GreetingController.java
@@ -1,23 +1,35 @@
 package com.application.shaverma;
 
+import com.application.shaverma.domain.Menu;
+import com.application.shaverma.repos.MenuRepo;
+import org.apache.logging.log4j.message.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MenuRepo menuRepo;
     @GetMapping("/Menu")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="Menu") String name, // id , imp, def val
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
+    public String menu(Map<String, Object> model) {
+        Iterable<Menu> message = menuRepo.findAll();
+        model.put("message", message);
         return "Menu";
     }
-
+    @PostMapping("/Menu")
+    public String addMenu(@RequestParam String name, @RequestParam String discription,Map<String, Object> model ) {
+       Menu mn =  new Menu(name, discription);
+       menuRepo.save(mn);
+        Iterable<Menu> message = menuRepo.findAll();        // need to reformat
+        model.put("message", message);                      // need to reformat
+       return "Menu";
+    }
+    // RequestParam забирает данные из формы при POST запросе и из URl при GET запросе
     @GetMapping("/")
     public String main(Map<String, Object> model) {
         model.put("some", "Welcome!");
--- src/main/java/com/application/shaverma/domain/Menu.java
+++ src/main/java/com/application/shaverma/domain/Menu.java
@@ -1,35 +1,31 @@
 package com.application.shaverma.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Menu {
     // need some annotations to add, for db connection
-    private int num;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+   // @GenericGenerator(name="system-uuid", strategy = "uuid") ?????????????
+
+    private int id;
     private String name;
     private String discription;
 
-    public int getNum() {
-        return num;
+
+    // При аннотации @Entity необходимо создать пустой конструктор, иначе будут проблему с созданием Bean данного класса
+
+    public Menu() {
     }
 
-    public void setNum(int num) {
-        this.num = num;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Menu(String name, String discription) {
         this.name = name;
-    }
-
-    public String getDiscription() {
-        return discription;
-    }
-
-    public void setDiscription(String discription) {
         this.discription = discription;
     }
+
 }
