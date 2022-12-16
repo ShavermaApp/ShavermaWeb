c70f9e35622082fa14d36d2316a6bab83c966018
Zaby2 <109461366+Zaby2@users.noreply.github.com>
2022-12-16 20:26:17 +0300

    all templates were fixed

--- src/main/java/com/application/shaverma/domain/Menu.java
+++ src/main/java/com/application/shaverma/domain/Menu.java
@@ -17,8 +17,30 @@ public class Menu {
     private String name;
     private String discription;
 
+    public int getId() {
+        return id;
+    }
 
-    // При аннотации @Entity необходимо создать пустой конструктор, иначе будут проблему с созданием Bean данного класса
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+// При аннотации @Entity необходимо создать пустой конструктор, иначе будут проблему с созданием Bean данного класса
 
     public Menu() {
     }
