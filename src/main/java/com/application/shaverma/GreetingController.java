14a7c123d1e26b9528a98dd317515deb041bfda9
Zaby2 <109461366+Zaby2@users.noreply.github.com>
2022-12-16 19:33:12 +0300

    iteration need to be fixed

--- src/main/java/com/application/shaverma/GreetingController.java
+++ src/main/java/com/application/shaverma/GreetingController.java
@@ -2,9 +2,9 @@ package com.application.shaverma;
 
 import com.application.shaverma.domain.Menu;
 import com.application.shaverma.repos.MenuRepo;
-import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,17 +16,17 @@ public class GreetingController {
     @Autowired
     private MenuRepo menuRepo;
     @GetMapping("/Menu")
-    public String menu(Map<String, Object> model) {
+    public String menu(Model model) {
         Iterable<Menu> message = menuRepo.findAll();
-        model.put("message", message);
+        model.addAttribute("message", message);
         return "Menu";
     }
     @PostMapping("/Menu")
-    public String addMenu(@RequestParam String name, @RequestParam String discription,Map<String, Object> model ) {
+    public String addMenu(@RequestParam String name, @RequestParam String discription,Model model ) {
        Menu mn =  new Menu(name, discription);
        menuRepo.save(mn);
         Iterable<Menu> message = menuRepo.findAll();        // need to reformat
-        model.put("message", message);                      // need to reformat
+        model.addAttribute("message", message);                 // need to reformat
        return "Menu";
     }
     // RequestParam забирает данные из формы при POST запросе и из URl при GET запросе
